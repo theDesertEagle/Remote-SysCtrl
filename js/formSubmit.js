389402a4378ab97dcb476f82aa0859b34d054319
Rachit Jas <racroxsinghiskinng10@gmail.com>
2018-06-22 01:10:26 -0500

    symbol and logo tweak

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,5 +1,8 @@
 var submitSymbol = document.getElementById('submit-symbol');
-var inputFields = document.getElementsByTagName('input'); 
+var inputFields = document.getElementsByTagName('input');
+var formFilled = false;
+var keyCode = null;
+
 function areInputFieldsFilled() {
 	if (inputFields[0].value != "" && inputFields[1].value != ""){
 		return true;
@@ -7,14 +10,21 @@ function areInputFieldsFilled() {
 	return false;
 }
 
-function checkFormValidity(){
-	if (areInputFieldsFilled() && window.getComputedStyle(submitSymbol).getPropertyValue('left') != "0"){
+function checkFormStatus(event){
+	keyCode = event.which || event.keyCode; //event.which used to support Firefox browser compatibility,
+	if (areInputFieldsFilled() && window.getComputedStyle(submitSymbol).getPropertyValue('left') != "0px"){ //Returned value in pixels
 		console.log("here");
+		console.log(window.getComputedStyle(submitSymbol).getPropertyValue('left'));
 		submitSymbol.style.color = "rgba(200, 200, 200, 0.9)";
-		submitSymbol.style.left = "0"; 
+		submitSymbol.style.left = "0rem";
+		formFilled = true; 
 	}
-	if (!areInputFieldsFilled() && window.getComputedStyle(submitSymbol).getPropertyValue('left') != "3rem"){
+	if (!areInputFieldsFilled() && window.getComputedStyle(submitSymbol).getPropertyValue('left') != "3rem"){ //Using known limitation
 		submitSymbol.style.color = "rgba(200, 200, 200, 0)";
-		submitSymbol.style.left = "3rem"; 		
+		submitSymbol.style.left = "3rem";
+		formFilled = false; 		
+	}
+	if (formFilled == true && keyCode == 13){
+		alert("route to server selection page");		
 	}
 }
