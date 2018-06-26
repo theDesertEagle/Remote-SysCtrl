d9dc3b98e59c2ff1670a2e83bbefd1af2b673036
Rachit Jas <racroxsinghiskinng10@gmail.com>
2018-06-25 21:47:44 -0500

    style change

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -11,16 +11,20 @@ function areInputFieldsFilled() {
 }
 
 function checkFormStatus(event){
+	console.log(event.target);
+
 	keyCode = event.which || event.keyCode; //event.which used to support Firefox browser compatibility,
 	if (areInputFieldsFilled() && window.getComputedStyle(submitSymbol).getPropertyValue('left') != "0px"){ //Returned value in pixels
 		console.log("here");
 		console.log(window.getComputedStyle(submitSymbol).getPropertyValue('left'));
-		submitSymbol.style.color = "rgba(200, 200, 200, 0.9)";
+		submitSymbol.style.color = "rgba(255, 109, 0, 0.9)";
+		submitSymbol.style.textShadow = "rgba(255, 204, 0, 0.9) 0 0 1rem"
 		submitSymbol.style.left = "0rem";
 		formFilled = true; 
 	}
 	if (!areInputFieldsFilled() && window.getComputedStyle(submitSymbol).getPropertyValue('left') != "3rem"){ //Using known limitation
 		submitSymbol.style.color = "rgba(200, 200, 200, 0)";
+		submitSymbol.style.textShadow = "rgba(255, 204, 0, 0) 0 0 0rem"
 		submitSymbol.style.left = "3rem";
 		formFilled = false; 		
 	}
